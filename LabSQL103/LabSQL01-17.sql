736c034fd209a12c32bd7f833ac1414056531512
Wa-542 <mkm211088@gmail.com>
2025-07-17 15:48:58 +0700

    Update LabSQL01-17.sql

--- LabSQL103/LabSQL01-17.sql
+++ LabSQL103/LabSQL01-17.sql
@@ -1,6 +1,5 @@
-
+SELECT * FROM Employees
+SELECT EmployeeID , FirstName , LastName FROM Employees
 SELECT * FROM Employees WHERE City = 'London'
---
 
-
-SELECT City, Country FROM Categories
+SELECT City, Country FROM Customers
