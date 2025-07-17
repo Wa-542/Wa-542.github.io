a8b79eef9dd3a302577313dd9a5b788383667458
Wa-542 <mkm211088@gmail.com>
2025-07-17 16:01:44 +0700

    Update LabSQL01-17.sql

--- LabSQL103/LabSQL01-17.sql
+++ LabSQL103/LabSQL01-17.sql
@@ -1,5 +1,11 @@
 SELECT * FROM Employees
+
 SELECT EmployeeID , FirstName , LastName FROM Employees
+
 SELECT * FROM Employees WHERE City = 'London'
 
 SELECT City, Country FROM Customers
+
+SELECT DISTINCT City, Country FROM Customers
+
+SELECT * FROM Products WHERE Unitprice > 200
